533148a66cdc6f84ba12660bae185ae1ee77184f
capu <hi@capu.tech>
2024-04-10 19:08:16 -0300

    andys naive traspose

--- keyboards/sentraq/s60_x/keymaps/capu/keymap.c
+++ keyboards/sentraq/s60_x/keymaps/capu/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LT(1,KC_TAB), KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
     KC_CAPS,      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT         , KC_ENT,  \
     KC_LSFT,              KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH         , KC_RSFT, \
-    KC_LCTL,     KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT,  KC_RGUI,  KC_RCTL    ,  MO(2) \
+    KC_LCTL,     KC_LGUI, KC_LALT,                            LT(3,KC_SPC),                             KC_RALT,  KC_RGUI,  KC_RCTL    ,  MO(2) \
   ),
 
   /* 1: Fn layer
@@ -74,5 +74,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, RGB_VAD, RGB_VAI, _______, _______, _______, _______, _______, _______ , _______, _______,          _______, \
     _______,          RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, _______, KC_MPRV , KC_MPLY, KC_MNXT,          _______, \
     _______, _______, _______,                            _______,                             _______, _______, _______, _______  \
-  )
+  ),
+
+  /* 0: Main layer
+   * both meta keys switch to the navigation layer.
+   * the app key switches to the meta/rarely used layer
+   * and tab, when held, activates the mouse layer.
+   * ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
+   * │ESC  │  1  │  2  │  3  │  4  │  5  │  6  │  7  │  8  │  9  │  0  │  -  │  =  │▒▒▒▒▒│BKSPC│
+   * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+   * │TAB/M│  Q  │  W  │  E  │  R  │  T  │  Y  │  U  │  I  │  O  │  P  │  [  │  ]  │  \  │█████│
+   * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+   * │CAPSL│  A  │  S  │  D  │  F  │  G  │  H  │  J  │  K  │  L  │  ;  │  '  │▒▒▒▒▒│ENTER│█████│
+   * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+   * │LSHFT│▒▒▒▒▒│  Z  │  X  │  C  │  V  │  B  │  N  │  M  │  ,  │  .  │  /  │▒▒▒▒▒│RSHFT│▒▒▒▒▒│
+   * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+   * │LCTRL│L_GUI│L_ALT│█████│█████│█████│ SPC │█████│█████│█████│R_ALT│ FN0 │ FN1 │RCTRL│█████│
+   * └─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
+   */
+  /* 0: mirrored ANSI for my crippled friend*/
+
+  [3] = LAYOUT_60_ansi(
+    KC_BSPC,    KC_EQL,  KC_MINS, KC_0,    KC_9,   KC_8,    KC_7,    KC_6, KC_5, KC_4, KC_3, KC_2,        KC_1,         KC_ESC,   \
+    KC_BSLS,     KC_RBRC, KC_LBRC, KC_P,    KC_O,   KC_I,    KC_U,    KC_Y, KC_T, KC_R, KC_E, KC_W,        KC_Q,         KC_TAB,         \
+    KC_ENT,      KC_QUOT, KC_SCLN, KC_L,    KC_K,   KC_J,    KC_H,    KC_G, KC_F, KC_D, KC_S, KC_A,        KC_CAPS,    \
+    KC_RSFT,     KC_SLSH, KC_DOT,  KC_COMM, KC_M,   KC_N,    KC_B,    KC_V, KC_C, KC_X, KC_Z, KC_LSFT,   \
+    _______ , KC_RCTL, KC_RGUI, KC_RALT, KC_SPC, KC_LALT, KC_LGUI, KC_LCTL \
+  ),
 };
